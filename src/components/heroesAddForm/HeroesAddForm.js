58bb704b53dc0a9527c13e426e9bebb692cd7c8d
Serj <sokolikserg@gmail.com>
2023-04-18 22:50:48 +0300

    2

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,4 +1,13 @@
 import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+import { useHttp } from '../../hooks/http.hook';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { heroesFetching, heroesFetched, heroesFetchingError } from '../../actions';
+import HeroesListItem from "../heroesListItem/HeroesListItem";
+import Spinner from '../spinner/Spinner';
 
 // Задача для этого компонента:
 // Реализовать создание нового героя с введенными данными. Он должен попадать
@@ -11,47 +20,84 @@ import { Formik, Field, Form, ErrorMessage } from 'formik';
 // данных из фильтров
 
 const HeroesAddForm = () => {
+    const { filters } = useSelector(state => state);
+    const dispatch = useDispatch();
+    const { request } = useHttp();
+
+    useEffect(() => {
+        dispatch(heroesFetching());
+        request("http://localhost:3001/heroes")
+            .then(data => dispatch(heroesFetched(data)))
+            .catch(() => dispatch(heroesFetchingError()))
+
+        // eslint-disable-next-line
+    }, []);
+
     return (
-        <form className="border p-4 shadow-lg rounded">
-            <div className="mb-3">
-                <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
-                <input 
-                    required
-                    type="text" 
-                    name="name" 
-                    className="form-control" 
-                    id="name" 
-                    placeholder="Как меня зовут?"/>
-            </div>
+        <Formik
+            initialValues={{
+                name: '',
+                text: '',
+                element: ''
+            }}
+            validationSchema={Yup.object({
+                name: Yup.string()
+                    .required('Это поле обязательно!'),
+                text: Yup.string()
+                    .required('Это поле обязательно!'),
+                element: Yup.string()
+                    .required('Это поле обязательно!')
+            })}
+            onSubmit={values => {
+                console.log(values);
+            }}
+        >
+            <Form className="border p-4 shadow-lg rounded">
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
+                    <Field
+                        required
+                        type="text"
+                        name="name"
+                        className="form-control"
+                        id="name"
+                        placeholder="Как меня зовут?" />
+                    <ErrorMessage component="div" className="error" name="name" />
+                </div>
 
-            <div className="mb-3">
-                <label htmlFor="text" className="form-label fs-4">Описание</label>
-                <textarea
-                    required
-                    name="text" 
-                    className="form-control" 
-                    id="text" 
-                    placeholder="Что я умею?"
-                    style={{"height": '130px'}}/>
-            </div>
+                <div className="mb-3">
+                    <label htmlFor="text" className="form-label fs-4">Описание</label>
+                    <Field
+                        required
+                        as="textarea"
+                        name="text"
+                        className="form-control"
+                        id="text"
+                        placeholder="Что я умею?"
+                        style={{ "height": '130px' }} />
+                    <ErrorMessage component="div" className="error" name="text" />
+                </div>
 
-            <div className="mb-3">
-                <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
-                <select 
-                    required
-                    className="form-select" 
-                    id="element" 
-                    name="element">
-                    <option >Я владею элементом...</option>
-                    <option value="fire">Огонь</option>
-                    <option value="water">Вода</option>
-                    <option value="wind">Ветер</option>
-                    <option value="earth">Земля</option>
-                </select>
-            </div>
+                <div className="mb-3">
+                    <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
+                    <Field
+                        required
+                        as="select"
+                        className="form-select"
+                        id="element"
+                        name="element">
+                        <option >Я владею элементом...</option>
+                        <option value="fire">Огонь</option>
+                        <option value="water">Вода</option>
+                        <option value="wind">Ветер</option>
+                        <option value="earth">Земля</option>
+                    </Field>
+                    <ErrorMessage component="div" className="error" name="element" />
+                </div>
 
-            <button type="submit" className="btn btn-primary">Создать</button>
-        </form>
+                <button type="submit" className="btn btn-primary">Создать</button>
+            </Form>
+        </Formik>
     )
 }
 
