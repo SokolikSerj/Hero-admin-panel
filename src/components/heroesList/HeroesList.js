a4d7f77bcad124e6bae324229a3b62046bb8d394
SokolikSerj <sokolikserg@gmail.com>
2023-04-18 12:58:18 +0300

    Commit 2

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,4 +1,4 @@
-import {useHttp} from '../../hooks/http.hook';
+import { useHttp } from '../../hooks/http.hook';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -12,9 +12,9 @@ import Spinner from '../spinner/Spinner';
 // Удаление идет и с json файла при помощи метода DELETE
 
 const HeroesList = () => {
-    const {heroes, heroesLoadingStatus} = useSelector(state => state);
+    const { heroes, heroesLoadingStatus } = useSelector(state => state);
     const dispatch = useDispatch();
-    const {request} = useHttp();
+    const { request } = useHttp();
 
     useEffect(() => {
         dispatch(heroesFetching());
@@ -26,9 +26,17 @@ const HeroesList = () => {
     }, []);
 
     if (heroesLoadingStatus === "loading") {
-        return <Spinner/>;
+        return <Spinner />;
     } else if (heroesLoadingStatus === "error") {
         return <h5 className="text-center mt-5">Ошибка загрузки</h5>
+    }
+
+    const deleteItem = (id) => {
+        request(`http://localhost:3001/heroes/${id}`, "DELETE")
+            .then(() => request("http://localhost:3001/heroes")
+                .then(data => dispatch(heroesFetched(data)))
+                .catch(() => dispatch(heroesFetchingError())));
+
     }
 
     const renderHeroesList = (arr) => {
@@ -36,8 +44,8 @@ const HeroesList = () => {
             return <h5 className="text-center mt-5">Героев пока нет</h5>
         }
 
-        return arr.map(({id, ...props}) => {
-            return <HeroesListItem key={id} {...props}/>
+        return arr.map(({ id, ...props }) => {
+            return <HeroesListItem key={id} deleteItem={() => deleteItem(id)} {...props} />
         })
     }
 
--- src/components/heroesListItem/HeroesListItem.js
+++ src/components/heroesListItem/HeroesListItem.js
@@ -1,7 +1,7 @@
-
-const HeroesListItem = ({name, description, element}) => {
+const HeroesListItem = ({ name, description, element, deleteItem }) => {
 
     let elementClassName;
+
 
     switch (element) {
         case 'fire':
@@ -21,19 +21,24 @@ const HeroesListItem = ({name, description, element}) => {
     }
 
     return (
-        <li 
+        <li
             className={`card flex-row mb-4 shadow-lg text-white ${elementClassName}`}>
-            <img src="http://www.stpaulsteinbach.org/wp-content/uploads/2014/09/unknown-hero.jpg" 
-                 className="img-fluid w-25 d-inline" 
-                 alt="unknown hero" 
-                 style={{'objectFit': 'cover'}}/>
+            <img src="http://www.stpaulsteinbach.org/wp-content/uploads/2014/09/unknown-hero.jpg"
+                className="img-fluid w-25 d-inline"
+                alt="unknown hero"
+                style={{ 'objectFit': 'cover' }} />
             <div className="card-body">
-                
+
                 <h3 className="card-title">{name}</h3>
                 <p className="card-text">{description}</p>
             </div>
             <span className="position-absolute top-0 start-100 translate-middle badge border rounded-pill bg-light">
-                <button type="button" className="btn-close btn-close" aria-label="Close"></button>
+                <button
+                    type="button"
+                    className="btn-close btn-close"
+                    aria-label="Close"
+                    onClick={deleteItem}
+                />
             </span>
         </li>
     )
